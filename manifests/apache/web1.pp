ef006391c1213073d655a5fe942128f2bb366d48
Mike Langhorst <mike.langhorst@gmail.com>
2015-01-02 17:37:46 -0800

    fixing syntax

--- manifests/apache/web1.pp
+++ manifests/apache/web1.pp
@@ -1,7 +1,7 @@
 class profile::apache::web1 {
-  apache_test {
-    $base_dir => 'app/apache/web1',
-    $package_state => 'latest',
-    $instance_name => 'web1',
+  apache_test { 'web1':
+    base_dir      => 'app/apache/web1',
+    package_state => 'latest',
+    instance_name => 'web1',
   }
 }
