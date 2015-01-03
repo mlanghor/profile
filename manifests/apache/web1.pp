e88955662873a3f936445967ae093df539dad307
Mike Langhorst <mike.langhorst@gmail.com>
2015-01-03 11:53:37 -0800

    adding hiera lookups

--- manifests/apache/web1.pp
+++ manifests/apache/web1.pp
@@ -1,7 +1,14 @@
 class profile::apache::web1 {
+  $base_dir = hiera('::apache_web1::base_dir')
+  $package_state = heira('::apache_web1::package_state')
+  $instance_name = hiera('::apache_web1::instance_name')
+
   apache_test { 'web1':
-    base_dir      => 'app/apache/web1',
-    package_state => 'latest',
-    instance_name => 'web1',
+    base_dir      => $base_dir,
+    package_state => $package_state,
+    instance_name => $instance_name,
   }
+
+    $web1_vhosts = hiera_hash('apache_web1::ssl_vhost',{})
+    create_resources('apache_test::ssl_vhost',$web1_vhosts)
 }
