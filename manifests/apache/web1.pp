18cc70ec825b58ba75de269465e94f20135d63a0
Mike Langhorst <mike.langhorst@gmail.com>
2015-01-05 20:12:35 -0800

    enabled instance_name

--- manifests/apache/web1.pp
+++ manifests/apache/web1.pp
@@ -1,12 +1,12 @@
 class profile::apache::web1 {
   $base_dir = hiera('apache_web1::base_dir')
   $package_state = hiera('apache_web1::package_state')
-  #$instance_name = hiera('apache_web1::instance_name')
+  $instance_name = hiera('apache_web1::instance_name')
 
   apache_test { 'web1':
     base_dir      => $base_dir,
     package_state => $package_state,
-    # instance_name => $instance_name,
+    instance_name => $instance_name,
   }
 
   #  $web1_vhosts = hiera_hash('apache_web1::ssl_vhost',{})
